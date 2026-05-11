d1c5b7bc41d52cad30f348f2bdf900f6f755146d
lukiburg <lukiburg@web.de>
2026-05-11 22:17:57 +0200

    Change output to be clearer, reformat class methods to functions

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ A small cli-tool to calculate the sum record for new solutions for Opus Magnum b
 - Use `pip install -r requirements.txt` to install required packages (globally or in a virtual env).
 - Put [om.py](http://critelli.technology/om.py) by panic next to this script (TODO: make a flag to specify location instead.)
 
-If you want windows-toasts to be displayed over your game, turn off "Do Not Disturb" and go to 
-**Settings > System > Notifications > Focus Assist** and turn off\
-"When I'm playing a game" and "When I'm using an app in full screen mode"
+If you want windows notifications to be displayed over your game, turn off "Do Not Disturb" and go to 
+**Settings > System > Notifications > Focus Assist** and turn off
+"When I'm playing a game" and "When I'm using an app in full screen mode".
+
 
 ## Usage
 Run `python om-sum-autocalc.py`. The script is now running and waiting for changes in the solutions directory
@@ -19,11 +20,11 @@ Run `python om-sum-autocalc.py`. The script is now running and waiting for chang
 Stop with `CTRL+C` 
 
 ## ToDo's
-In no specific order. Feel free to open issues if you have any suggestions or find bugs.
+In no specific order. Feel free to open issues if you have any suggestions or found a bug.
 
 - Add flag to specify om.py location
 - Add flag to disable toasts
 - Support for linux (feel free to fork for mac)
 - Compare to other local solutions
-- Auto-check against online leaderboard for new record
+- Create small image instead of text for colored deltas (not supported in windows notification texts)
 
--- om-sum-autocalc.py
+++ om-sum-autocalc.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Tuple
 
 import requests
-from requests import HTTPError
+from requests import HTTPError, ConnectionError
 from watchdog.events import FileSystemEventHandler, DirModifiedEvent, FileModifiedEvent, RegexMatchingEventHandler
 from watchdog.observers import Observer
 from win11toast import toast
@@ -17,14 +17,82 @@ import om
 
 
 WINDOWS_APP_ID = "Opus Magnum Sum Calculator"
+ONLINE_TIMEOUT = 2.0
+
+
+def extract_puzzle_name(solution_filename: str) -> str:
+    # TODO: can I get the display_name from om somehow? Fetching from https://zlbb.faendir.com/swagger-ui seems overkill
+    # but I need it to calculate local best
+    match = re.match("([a-zA-Z]*-?)*", solution_filename)
+    if match is None:
+        raise ValueError(f"Invalid solution name: {solution_filename} -> no puzzle name extractable.")
+    text_only = match.group(0)[:-1].replace("-", " ")
+    return text_only.title()
+
+
+def solution_sum(solution: om.Solution) -> int:
+    sum3 = solution.cycles + solution.cost + solution.area
+    #sum4 = sum3 + solution.instructions
+    return sum3
+
+
+def create_delta_dict(solution: om.Solution, delta_solution: om.Solution):
+    return {
+        'score': solution_sum(delta_solution),
+        'delta': solution_sum(solution) - solution_sum(delta_solution),
+        'cost': solution.cost - delta_solution.cost,
+        'cycles': solution.cycles - delta_solution.cycles,
+        'area': solution.area - delta_solution.area
+    }
+
+
+def get_online_url(for_puzzle: str) -> str:
+    return f"https://zlbb.faendir.com/om/puzzle/{for_puzzle}/category/SUM/record"
+
+
+def fetch_online(url: str) -> om.Solution | None:
+    online_record_request = requests.get(
+        url, headers={"accept": "application/json"}, timeout=ONLINE_TIMEOUT)
+    if online_record_request.status_code == 200:
+        response = online_record_request.json()
+        online_score = response['score']
+        artificial_online_solution = om.Solution()
+        artificial_online_solution.cycles = online_score['cycles']
+        artificial_online_solution.area = online_score['area']
+        artificial_online_solution.cost = online_score['cost']
+        artificial_online_solution.instructions = online_score['instructions']
+        return artificial_online_solution
+    else:
+        print(f"Warning: Could not fetch online record ({online_record_request.status_code})\n {online_record_request}")
+        return None
+
+
+def format_output(solution, deltas):
+    part1 = f"Sum3:\t{solution_sum(solution)}\t\t{solution.cost}g/{solution.cycles}c/{solution.area}a"
+    strings = [part1]
+    for name, delta in deltas.items():
+        s = f"\u0394{name}:\t{delta['score']}({delta['delta']:+})  \t{delta['cost']:+}g/{delta['cycles']:+}c/{delta['area']:+}a"
+        strings.append(s)
+    return "\n".join(strings)
+
+
+def windows_toast(title, output):
+    toast(f"{title}", output, scenario="reminder", duration="long",
+          audio={'silent': 'true'},
+          app_id=WINDOWS_APP_ID,
+          on_dismissed=lambda *args: None)  # to suppress timeout notification in console
 
 
 class ChangeHandler(FileSystemEventHandler):
     def __init__(self):
         self.last_solution: om.Solution = om.Solution()
 
+    def test(self):
+        print( 'test')
+
     def on_modified(self, event: DirModifiedEvent | FileModifiedEvent) -> None:
-        if event.event_type == ("modified") and not event.src_path.endswith("save.dat"):      # trigger only once
+        # print(event)
+        if event.event_type == ("modified") and event.src_path.endswith("solution"):      # trigger only once
             self.calculate_new_sum(event.src_path)
 
     def calculate_new_sum(self, path: str) -> om.Solution | None:
@@ -38,93 +106,29 @@ class ChangeHandler(FileSystemEventHandler):
                 )):
                 solution_filename = Path(path).name
                 puzzle_name = extract_puzzle_name(solution_filename)
-                sum3 = self.calculate_sum(solution)
-                output_strings = []
-                if self.last_solution.solved:
-                    delta_last = self.calculate_delta(solution, self.last_solution)
-                    delta_last_expanded = self.calculate_delta_expanded(solution, self.last_solution)
-                    string = self.format_output_delta(solution, self.last_solution, sum3, delta_last, delta_last_expanded)
-                    output_strings.append(string)
-                best_solution = self.find_best(solution)
-                delta_best = self.calculate_delta(solution, best_solution)
-                delta_best_expanded = self.calculate_delta_expanded(solution, best_solution)
-                output_string = self.format_output_delta(solution, best_solution, sum3, delta_best, delta_best_expanded)
-                # print(output_string)
-                output_strings.append(output_string)
-                output = "\n".join(output_strings)
-                # print(output)
-                toast(f"{puzzle_name}", output, scenario="reminder", duration="long",
-                      audio=True,  # results in silent?
-                      app_id=WINDOWS_APP_ID,
-                      on_dismissed=lambda *args: None)  # to suppress timeout notification in console
-
+                output = self.create_output(solution)
+                print(output, end="\n\n")
+                windows_toast(puzzle_name, output)
                 self.last_solution = solution
                 self.last_solution.source_name = 'last solve'
-        except ValueError as e:
-            print(f'om.py threw an Exception, probably harmless: {e}')
-            # om.py shows a value error if script is started before Opus Magnum.
         except PermissionError as e:
             print(f'Tried to open file while still in use by system: {path}')
         return None
 
-    def calculate_sum(self, solution: om.Solution) -> int:
-        sum3 = solution.cycles + solution.cost + solution.area
-        #sum4 = sum3 + solution.instructions
-        return sum3
+    def create_output(self, solution: om.Solution) -> str:
+        deltas = {}
+        if self.last_solution.solved:
+            deltas['last'] = create_delta_dict(solution, self.last_solution)
+        # deltas['best'] = self.create_delta_dict(solution, best_solution)
+        try:
+            online_solution = fetch_online(get_online_url(solution.puzzle.decode()))
+            deltas['online'] = create_delta_dict(solution, online_solution)
+        except ConnectionError as e:
+            print(f"Could not fetch online: {e}")
+        return format_output(solution, deltas)
 
-    def calculate_delta(self, solution1: om.Solution, solution2: om.Solution) -> int:
-        return self.calculate_sum(solution1) - self.calculate_sum(solution2)
-
-    def calculate_delta_expanded(self, solution1: om.Solution, solution2: om.Solution) -> Tuple[int, int, int]:
-        sum3_delta = (solution1.cost - solution2.cost, solution1.cycles - solution2.cycles, solution1.area - solution2.area)
-        #sum4_delta = sum3_delta + (solution1.instructions - solution2.instructions)
-        return sum3_delta
-
-    def find_best(self, solution: om.Solution) -> om.Solution:
-        online = self.fetch_online(solution.puzzle.decode())
-        if online:
-            field = [online]
-        else:
-            raise HTTPError("Could not request online")
-        return field[0]
-
-    def fetch_online(self, for_puzzle: str) -> om.Solution | None:
-        online_record_request = requests.get(
-            f"https://zlbb.faendir.com/om/puzzle/{for_puzzle}/category/SUM/record",
-            headers={"accept": "application/json"})
-        if online_record_request.status_code == 200:
-            response = online_record_request.json()
-            online_score = response['score']
-            artificial_online_solution = om.Solution()
-            artificial_online_solution.cycles = online_score['cycles']
-            artificial_online_solution.area = online_score['area']
-            artificial_online_solution.cost = online_score['cost']
-            artificial_online_solution.instructions = online_score['instructions']
-            artificial_online_solution.source_name = "online"
-            artificial_online_solution.link = f"https://zlbb.faendir.com/puzzles/{for_puzzle}/records"
-            return artificial_online_solution
-        else:
-            print(f"Warning: Could not fetch online record ({online_record_request.status_code})\n {online_record_request}")
-            return None
-
-    def format_output_delta(self, solution, comparing_to_solution, sum3, delta, delta_expanded):
-        return (f"Sum3: {sum3} ({delta:+}) \t\t\u0394{comparing_to_solution.source_name}\n"
-                f"{solution.cost}g({delta_expanded[0]:+})/{solution.cycles}c({delta_expanded[1]:+})"
-                f"/{solution.area}a({delta_expanded[2]:+})")
-
-    def format_output(self, solution, sum3):
-        return (f"Sum3: {sum3}\n"
-                f"{solution.cost}g/{solution.cycles}c/{solution.area}a")
-
-def extract_puzzle_name(solution_filename: str) -> str:
-    # TODO: can I get the display_name from om somehow? Fetching from https://zlbb.faendir.com/swagger-ui seems overkill
-    # but I need it to calculate local best
-    match = re.match("([a-zA-Z]*-?)*", solution_filename)
-    if match is None:
-        raise ValueError(f"Invalid solution name: {solution_filename} -> no puzzle name extractable.")
-    text_only = match.group(0)[:-1].replace("-", " ")
-    return text_only.title()
-
+    def find_local_best(self, solution: om.Solution) -> om.Solution:
+       raise NotImplementedError
 
 
 if __name__ == '__main__':
